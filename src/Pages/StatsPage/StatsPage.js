d307ba69667be1efdc70f48b9ab14262a5905a44
legend524 <thecrazydev524@gmail.com>
2023-06-25 12:14:39 +0900

    added stats for chains

--- src/Pages/StatsPage/StatsPage.js
+++ src/Pages/StatsPage/StatsPage.js
@@ -1,108 +1,106 @@
-import React, { useEffect, useState } from 'react'
-import Header from '../../components/Header/Header'
-import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
-import { Container, Grid, Typography } from '@mui/material'
-import Skeleton from '@mui/material/Skeleton'
-import moment from 'moment'
-import './styles.scss'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Header from "../../components/Header/Header";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer
+} from "recharts";
+import { Container, Grid, Typography } from "@mui/material";
+import { useNetwork } from "wagmi";
+import { initTradeInfo } from "../../redux/actions";
+
+import Skeleton from "@mui/material/Skeleton";
+import moment from "moment";
+import { PROTOCOLS } from "../../environment/config";
+import "./styles.scss";
+import defaultImg from "../../assets/erc20.png";
 
 const StatsPage = () => {
-  const [TVLhistory, setTVLhistory] = useState([])
-  const [dexTVLData, setDexTVLData] = useState([])
+  const dispatch = useDispatch();
+  const { chain } = useNetwork();
+  let tradeInfo = useSelector((RootState) => RootState.trade);
 
-  const dexList = [
-    {
-      protocal: 'syncswap',
-      logo: 'https://icons.llama.fi/syncswap.png',
-      url: 'https://syncswap.xyz',
-    },
-    {
-      protocal: 'velocore',
-      logo: 'https://icons.llama.fi/velocore.png',
-      url: 'https://velocore.xyz',
-    },
-    {
-      protocal: 'gemswap',
-      logo: 'https://icons.llama.fi/gemswap.png',
-      url: 'https://zks.gemswap.online/swap',
-    },
-    {
-      protocal: 'mute.io',
-      logo: 'https://icons.llama.fi/mute.io.jpg',
-      url: 'https://app.mute.io',
-    },
-    {
-      protocal: 'SpaceFi',
-      logo: 'https://icons.llama.fi/spacefi.png',
-      url: 'https://spacefi.io',
-    },
-  ]
+  const [TVLhistory, setTVLhistory] = useState([]);
+  const [dexTVLData, setDexTVLData] = useState([]);
+  const addDefaultImg = (e) => {
+    e.target.src = defaultImg;
+  };
+  const getTVL = async (protocol) => {
+    const response = await fetch(
+      `https://api.llama.fi/v2/historicalChainTvl/${protocol}`
+    );
+    const result = await response.json();
+    setTVLhistory(result);
+  };
 
-  const getTVL = async () => {
-    const response = await fetch('https://api.llama.fi/v2/historicalChainTvl/zkSync%20Era')
-    const result = await response.json()
-    setTVLhistory(result)
-  }
-
-  const getDexTVL = async () => {
-    const dexData = await Promise.all(
-      dexList.map(async (dex) => {
-        const response = await fetch(`https://api.llama.fi/tvl/${dex.protocal}`)
-        const result = await response.json()
-        return {
-          ...dex,
-          tvl: result,
-        }
-      })
-    )
-    setDexTVLData(dexData)
-  }
+  const getDexTVL = async (protocol) => {
+    const response = await fetch(
+      `https://api.llama.fi/overview/dexs/${protocol}`
+    );
+    const result = await response.json();
+    const dexList = result.protocols.sort((a, b) => {
+      return b.totalAllTime - a.totalAllTime;
+    });
+    setDexTVLData(dexList.slice(0, 5));
+  };
 
   const convertCurrency = (labelValue) => {
-    return Math.abs(Number(labelValue)) >= 1.0e9
-      ? (Math.abs(Number(labelValue)) / 1.0e9).toFixed(2) + 'B'
-      : Math.abs(Number(labelValue)) >= 1.0e6
-      ? (Math.abs(Number(labelValue)) / 1.0e6).toFixed(2) + 'M'
-      : Math.abs(Number(labelValue)) >= 1.0e3
-      ? (Math.abs(Number(labelValue)) / 1.0e3).toFixed(2) + 'K'
-      : Math.abs(Number(labelValue))
-  }
+    return Number(labelValue) >= 1.0e9
+      ? (Number(labelValue) / 1.0e9).toFixed(2) + "B"
+      : Number(labelValue) >= 1.0e6
+      ? (Number(labelValue) / 1.0e6).toFixed(2) + "M"
+      : Number(labelValue) >= 1.0e3
+      ? (Number(labelValue) / 1.0e3).toFixed(2) + "K"
+      : Number(labelValue);
+  };
 
   const dateFormatter = (time) => {
-    var d = new Date(0)
-    d.setUTCSeconds(time)
-    return moment(d).format('DD/MM')
-  }
+    var d = new Date(0);
+    d.setUTCSeconds(time);
+    return moment(d).format("DD/MM");
+  };
 
   const calcChange = () => {
     return (
       1 -
-      Number(TVLhistory[TVLhistory.length - 2].tvl) / Number(TVLhistory[TVLhistory.length - 1].tvl)
-    ).toFixed(3)
-  }
-
+      Number(TVLhistory[TVLhistory.length - 2].tvl) /
+        Number(TVLhistory[TVLhistory.length - 1].tvl)
+    ).toFixed(3);
+  };
+  const initTradeState = (chainID) => {
+    console.log("dispatching action...");
+    dispatch(initTradeInfo(chainID));
+  };
   useEffect(() => {
-    getTVL()
-    getDexTVL()
-  }, [])
-
+    getTVL(PROTOCOLS[chain.id]);
+    getDexTVL(PROTOCOLS[chain.id].toLowerCase());
+    initTradeState(chain.id);
+  }, [chain]);
   return (
     <>
       <Header />
       <Container className='content-wrapper'>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Typography variant='h3'>zkSync era stats</Typography>
+            <Typography variant='h3'>
+              {PROTOCOLS[tradeInfo.chainId]} Stats
+            </Typography>
           </Grid>
           <Grid item xs={12} md={3}>
-            <Grid container direction={'column'}>
+            <Grid container direction={"column"}>
               <Grid className='grid-wrapper'>
                 <Typography variant='h6'>TVL (USD)</Typography>
                 <Typography variant='h4'>
                   {TVLhistory.length ? (
-                    <>${convertCurrency(TVLhistory[TVLhistory.length - 1].tvl)}</>
+                    <>
+                      ${convertCurrency(TVLhistory[TVLhistory.length - 1].tvl)}
+                    </>
                   ) : (
-                    <Skeleton variant='rounded' sx={{ bgcolor: '#2a3454' }} />
+                    <Skeleton variant='rounded' sx={{ bgcolor: "#2a3454" }} />
                   )}
                 </Typography>
               </Grid>
@@ -110,12 +108,15 @@ const StatsPage = () => {
               <Grid className='grid-wrapper'>
                 <Typography variant='h6'>Change (24h)</Typography>
                 {TVLhistory.length ? (
-                  <Typography variant='h4' className={calcChange() < 0 ? 'down' : 'up'}>
+                  <Typography
+                    variant='h4'
+                    className={calcChange() < 0 ? "down" : "up"}
+                  >
                     {calcChange()}%
                   </Typography>
                 ) : (
                   <Typography variant='h4'>
-                    <Skeleton variant='rounded' sx={{ bgcolor: '#2a3454' }} />
+                    <Skeleton variant='rounded' sx={{ bgcolor: "#2a3454" }} />
                   </Typography>
                 )}
               </Grid>
@@ -134,27 +135,39 @@ const StatsPage = () => {
                       top: 0,
                       right: 5,
                       left: 0,
-                      bottom: 0,
+                      bottom: 0
                     }}
                   >
                     <XAxis
                       dataKey='date'
                       tickFormatter={dateFormatter}
-                      tick={{ fill: 'white' }}
-                      tickLine={{ stroke: 'white' }}
+                      tick={{ fill: "white" }}
+                      tickLine={{ stroke: "white" }}
                     />
                     <YAxis
                       tickFormatter={convertCurrency}
                       orientation='right'
-                      tick={{ fill: 'white' }}
-                      tickLine={{ stroke: 'white' }}
+                      tick={{ fill: "white" }}
+                      tickLine={{ stroke: "white" }}
                     />
-                    <Tooltip formatter={convertCurrency} labelFormatter={dateFormatter} />
-                    <Area type='monotone' dataKey='tvl' stroke='#279778' fill='#234b56' />
+                    <Tooltip
+                      formatter={convertCurrency}
+                      labelFormatter={dateFormatter}
+                    />
+                    <Area
+                      type='monotone'
+                      dataKey='tvl'
+                      stroke='#279778'
+                      fill='#234b56'
+                    />
                   </AreaChart>
                 </ResponsiveContainer>
               ) : (
-                <Skeleton variant='rounded' sx={{ bgcolor: '#2a3454' }} height={200} />
+                <Skeleton
+                  variant='rounded'
+                  sx={{ bgcolor: "#2a3454" }}
+                  height={200}
+                />
               )}
             </Grid>
           </Grid>
@@ -162,12 +175,19 @@ const StatsPage = () => {
           <Grid item xs={12}>
             <Typography variant='h6'>TVL Rankings</Typography>
             <Grid className='grid-wrapper dex-ranking'>
-              <table className='dex-table' border='0' cellSpacing='0' cellPadding='0'>
+              <table
+                className='dex-table'
+                border='0'
+                cellSpacing='0'
+                cellPadding='0'
+              >
                 <thead>
                   <tr>
                     <th>No</th>
                     <th>Name</th>
-                    <th>Url</th>
+                    <th>1D change</th>
+                    <th>1w change</th>
+                    <th>1m change</th>
                     <th>TVL</th>
                   </tr>
                 </thead>
@@ -176,52 +196,79 @@ const StatsPage = () => {
                   <tbody>
                     {dexTVLData.map((data, index) => {
                       return (
-                        <tr key={index}>
+                        <tr
+                          key={index}
+                          className={index % 2 === 0 ? "table-hover" : ""}
+                        >
                           <td>{index + 1}</td>
                           <td className='dex-name'>
-                            <img src={data.logo} alt='dex icon' className='dex-icon' />
-                            {data.protocal}
+                            <img
+                              src={data.logo}
+                              alt='dex icon'
+                              className='dex-icon'
+                              onError={(e) => addDefaultImg(e)}
+                            />
+                            {data.displayName}
                           </td>
-                          <td>
-                            <a
-                              href={data.url}
-                              target='_blank'
-                              rel='noreferrer'
-                              className='dex-link'
-                            >
-                              {data.url}
-                            </a>
+                          <td className={data.change_1d > 0 ? "up" : "down"}>
+                            ${convertCurrency(data.change_1d)}
                           </td>
-                          <td>${convertCurrency(data.tvl)}</td>
+                          <td className={data.change_7d > 0 ? "up" : "down"}>
+                            ${convertCurrency(data.change_7d)}
+                          </td>
+                          <td className={data.change_1m > 0 ? "up" : "down"}>
+                            ${convertCurrency(data.change_1m)}
+                          </td>
+                          <td>${convertCurrency(data.totalAllTime)}</td>
                         </tr>
-                      )
+                      );
                     })}
                   </tbody>
                 ) : (
                   <tbody className='skeleton-table'>
                     <tr>
                       <td colSpan={4}>
-                        <Skeleton variant='rounded' sx={{ bgcolor: '#2a3454' }} height={40} />
+                        <Skeleton
+                          variant='rounded'
+                          sx={{ bgcolor: "#2a3454" }}
+                          height={40}
+                        />
                       </td>
                     </tr>
                     <tr>
                       <td colSpan={4}>
-                        <Skeleton variant='rounded' sx={{ bgcolor: '#2a3454' }} height={40} />
+                        <Skeleton
+                          variant='rounded'
+                          sx={{ bgcolor: "#2a3454" }}
+                          height={40}
+                        />
                       </td>
                     </tr>
                     <tr>
                       <td colSpan={4}>
-                        <Skeleton variant='rounded' sx={{ bgcolor: '#2a3454' }} height={40} />
+                        <Skeleton
+                          variant='rounded'
+                          sx={{ bgcolor: "#2a3454" }}
+                          height={40}
+                        />
                       </td>
                     </tr>
                     <tr>
                       <td colSpan={4}>
-                        <Skeleton variant='rounded' sx={{ bgcolor: '#2a3454' }} height={40} />
+                        <Skeleton
+                          variant='rounded'
+                          sx={{ bgcolor: "#2a3454" }}
+                          height={40}
+                        />
                       </td>
                     </tr>
                     <tr>
                       <td colSpan={4}>
-                        <Skeleton variant='rounded' sx={{ bgcolor: '#2a3454' }} height={40} />
+                        <Skeleton
+                          variant='rounded'
+                          sx={{ bgcolor: "#2a3454" }}
+                          height={40}
+                        />
                       </td>
                     </tr>
                   </tbody>
@@ -232,7 +279,7 @@ const StatsPage = () => {
         </Grid>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default StatsPage
+export default StatsPage;
